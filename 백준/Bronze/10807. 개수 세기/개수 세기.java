0f4e3596bf3e782ee4bf5656aafa92f77090b370
김도균 <79688062+dlfjs0472@users.noreply.github.com>
2023-02-01 01:35:53 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 17844 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -4,10 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] V = new int[201];
-        for (int i = 0; i<N; i++)
-            V[sc.nextInt()+100]++;
-        System.out.print(V[sc.nextInt()+100]);
+        int N = sc.nextInt();	//첫 입력받은 수(반복횟수)
+        int[] V = new int[N];
+        for (int i = 0; i <N; i++)
+		{
+			V[i] = sc.nextInt();
+		}
+		int target = sc.nextInt();
+		int cnt=0;
+		for(Integer num : V){
+			if(num == target)
+			{
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		sc.close();
     }
+
 }
